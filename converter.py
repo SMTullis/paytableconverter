a3aab66a6f0c0bf51871793643f78fdde39fde75
Sean M Tullis <stullis@fs.fed.us>
2019-02-20 08:09:48 -0700

    Initial commit

---
+++ converter.py
@@ -0,0 +1,23 @@
+import csv
+import decimal
+import io
+
+class PayTable:
+
+    def __init__(self, *header):
+        data = [header, ]
+
+    def append(self, *row):
+        data.append(row)
+
+
+def get_data(filepath):
+    with io.open("C:/Users/stullis/Downloads/paytables.csv", mode="r") as file:
+        data = [line for line in csv.reader(file)]
+
+    return data
+
+
+
+def main():
+    pass
